9436a8107776d332258c9a6dd8f186aca4e29fa0
Tim Anzigale <anzigalet@gmail.com>
2024-06-18 10:46:04 +0300

    added certificates

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+// SSL Certificate Paths
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH || "";
+const SSL_CRT_PATH = process.env.SSL_CRT_PATH || "";
 
 const app = express();
 
@@ -25,6 +34,33 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
+// Get SSL Documents
+let privateKey, certificate;
+
+try {
+  privateKey = fs.readFileSync(SSL_KEY_PATH, 'utf8');
+} catch (err) {
+  console.error(`Error reading private key from ${SSL_KEY_PATH}:`, err.message);
+  process.exit(1);
+}
+
+try {
+  certificate = fs.readFileSync(SSL_CRT_PATH, 'utf8');
+} catch (err) {
+  console.error(`Error reading certificate from ${SSL_CRT_PATH}:`, err.message);
+  process.exit(1);
+}
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+// Create HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+httpsServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
